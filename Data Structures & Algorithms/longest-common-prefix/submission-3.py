3c2ccaf13ffc9dc95fcb76a5c55a5d57f20751eb
anthony <52847839+anthonydwan@users.noreply.github.com>
2026-08-03 22:05:25 +1000

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        i = 0
+
+        strs.sort(key=len)
+        while True:
+            if i >= len(strs[0]):
+                return res
+            letter = strs[0][i]
+            for word in strs:
+                if len(word) <= i or word[i] != letter:
+                    return res
+            i +=1
+            res += letter
